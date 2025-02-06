772622a2ef595cf905ed1a90849ff38a8cbd509a
dhananjayaDev <d.dhananjaya@outlook.com>
2025-02-07 04:36:16 +0530

    navbar added

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import { FloatingNav } from "@/components/ui/floating-navbar";
 import { FaHome } from "react-icons/fa";
 import Grid from "@/components/Grid";
+import { navItems } from "@/data";
 // import { BentoGridDemo } from "@/components/BentoGridDemo";
 
 
@@ -12,9 +13,7 @@ export default function Home() {
     justify-center min-h-screen text-white overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <FloatingNav 
-          navItems={[
-            { name: "Home", link: "#", icon: <FaHome/> },
-          ]}
+          navItems={navItems}
         />
         <Hero />
         <Grid />
